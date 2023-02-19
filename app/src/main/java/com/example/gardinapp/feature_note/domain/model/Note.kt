be5e77c768ff64d97a89390a48c6c8e3697af94f
Rafik Mastouri <rafik.mastouri@esprit.tn>
2023-02-19 20:31:05 +0100

    first commit

---
+++ app/src/main/java/com/example/gardinapp/feature_note/domain/model/Note.kt
@@ -0,0 +1,3 @@
+package com.example.gardinapp.feature_note.domain.model
+
+data class Note()
---
+++ app/src/main/java/com/example/gardinapp/feature_note/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gardinapp.feature_note.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        }
+    }
+}
